a77b17f02affb2f6292697f1ae8e8f19ca4eb1d4
GabrielGene <gabrielgene97@gmail.com>
2019-03-11 12:24:01 -0300

    Add dynamic date

--- src/global.js
+++ src/global.js
@@ -1,21 +1,35 @@
+const date = new Date();
+const formattedDate = date
+  .toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  })
+  .replace(/ /g, ' de ');
+
+const hour = date.getHours();
+const minute = date.getMinutes();
+const ampm = hour >= 12 ? 'PM' : 'AM';
+
+const formattedHour = `${hour}:${minute} ${ampm}`;
 export default {
-	user: {
-		avatar: 'images/bolso.jpg',
-		fullName: 'Jair M. Bolsonaro',
-		twitter: '@jairbolsonaro'
-	},
-	datetime: '04:26 - 6 de mar de 2019',
-	retweets: 65.102,
-	likes: 157.243,
-	comments: 37,
-	avatars: [
-		'images/avatar-1.jpg',
-		'images/avatar-2.jpg',
-		'images/avatar-3.jpg',
-		'images/avatar-4.jpg',
-		'images/avatar-5.jpg',
-		'images/avatar-6.jpg',
-		'images/avatar-7.jpg',
-		'images/avatar-8.jpg'
-	]
+  user: {
+    avatar: 'images/bolso.jpg',
+    fullName: 'Jair M. Bolsonaro',
+    twitter: '@jairbolsonaro',
+  },
+  datetime: `${formattedHour} - ${formattedDate}`,
+  retweets: 65.102,
+  likes: 157.243,
+  comments: 37,
+  avatars: [
+    'images/avatar-1.jpg',
+    'images/avatar-2.jpg',
+    'images/avatar-3.jpg',
+    'images/avatar-4.jpg',
+    'images/avatar-5.jpg',
+    'images/avatar-6.jpg',
+    'images/avatar-7.jpg',
+    'images/avatar-8.jpg',
+  ],
 };
